90278d39ce838845ae35c88026592b49b764dc1b
elynxd <eliandrodviana@gmail.com>
2025-07-24 22:11:44 -0300

    fix: grid-cols to small screens and some new ajusts in the about page.

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -4,56 +4,57 @@ export default function About() {
     return (
         <div className="container mx-auto pt-24 text-white">
             <h1 className="mb-8 text-center font-bold text-4xl tracking-wide">Sobre mim</h1>
-            <div className="grid grid-cols-1 gap-8 align-baseline md:grid-cols-1">
+            <div className="grid grid-cols-1 gap-8 p-4 align-baseline md:grid-cols-1">
                 <div>
                     <h2 className="mb-4 font-bold text-2xl">Quem Sou Eu</h2>
                     <p>
                         Sou um DevOps e Desenvolvedor apaixonado, com amor por construir e implantar
                         aplicações web escaláveis e confiáveis. Tenho experiência em todos os
                         aspectos do ciclo de vida do desenvolvimento de software, desde o
-                        planejamento e design até a implementação e implantação.
+                        planejamento e design até a implementação e implantação em Clouds providers
+                        como AWS e Oracle.
                     </p>
                 </div>
                 <div>
                     <h2 className="mb-4 font-bold text-2xl">Minhas Habilidades</h2>
-                    <div className="grid grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
                         <Card>
-                            <CardHeader>
-                                <CardTitle>Frontend</CardTitle>
+                            <CardHeader className="flex justify-center">
+                                <CardTitle className="text-lime-300">Frontend</CardTitle>
                             </CardHeader>
                             <CardContent>
                                 <ul className="list-inside list-none">
                                     <li>React.js</li>
                                     <li>React Router</li>
                                     <li>TypeScript</li>
-                                    <li>Zustand</li>
                                     <li>Tailwind CSS</li>
                                     <li>Framer Motion</li>
                                 </ul>
                             </CardContent>
                         </Card>
                         <Card>
-                            <CardHeader>
-                                <CardTitle>Backend</CardTitle>
+                            <CardHeader className="flex justify-center">
+                                <CardTitle className="text-lime-300">Backend</CardTitle>
                             </CardHeader>
                             <CardContent>
                                 <ul className="list-inside list-none">
-                                    <li>SpringBoot 3</li>
+                                    <li>Java- SpringBoot 3</li>
                                     <li>Golang</li>
-                                    <li>PostgreSQL & DynamoDB</li>
+                                    <li>PostgreSQL, MySQL, MongoDB & DynamoDB</li>
                                     <li>Docker</li>
                                 </ul>
                             </CardContent>
                         </Card>
                         <Card>
-                            <CardHeader>
-                                <CardTitle>DevOps</CardTitle>
+                            <CardHeader className="flex justify-center">
+                                <CardTitle className="text-lime-300">DevOps</CardTitle>
                             </CardHeader>
                             <CardContent>
                                 <ul className="list-inside list-none">
+                                    <li>Github Actions</li>
                                     <li>CI/CD Pipelines</li>
-                                    <li>Kubernetes</li>
-                                    <li>AWS</li>
+                                    <li>AWS Cloud</li>
+                                    <li>Oracle Cloud - OCI</li>
                                     <li>Terraform</li>
                                 </ul>
                             </CardContent>
